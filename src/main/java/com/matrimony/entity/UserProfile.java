5c14dde7159f6fbcdde10e08dc9a084be16c36d2
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-06-07 12:40:07 +0530

    User profile entity dto

---
+++ src/main/java/com/matrimony/entity/UserProfile.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package com.matrimony.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Entity to store User Profile
+ * 
+ * @author user1
+ *
+ */
+@Entity
+@Table
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserProfile implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "First_Name")
+	private String firstName;
+
+	@Column(name = "Last_Name")
+	private String lastName;
+
+	@Column(name = "Mobile")
+	private Long mobile;
+
+	@Column(name = "City")
+	private String city;
+
+	@Column(name = "Montyhly_Income")
+	private Double monthlyIncome;
+
+	@Column(name = "Gender")
+	private String gender;
+
+	@Column(name = "Religion")
+	private String religion;
+
+	@Column(name = "Qualification")
+	private String qualification;
+
+	@Column(name = "User_Name")
+	private String userName;
+
+	@Column(name = "Password")
+	private String password;
+
+	@Column(name = "Email_Id")
+	private String emailId;
+
+	@Column(name = "Food_Habits")
+	private String foodHabit;
+
+	@Column(name = "Age")
+	private Integer age;
+
+}
