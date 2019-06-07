af18e21a5b7710426b36068c8fbebdd327b9e658
Malakayya <nandikolm@gmail.com>
2019-06-07 12:49:37 +0530

    Controller

--- src/main/java/com/matrimony/repository/MatrimonyRepository.java
+++ src/main/java/com/matrimony/repository/MatrimonyRepository.java
@@ -3,7 +3,9 @@ package com.matrimony.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.matrimony.entity.UserProfile;
+
 @Repository
-public interface MatrimonyRepository extends JpaRepository<Integer, Integer>{
+public interface MatrimonyRepository extends JpaRepository<UserProfile, Long>{
 
 }
