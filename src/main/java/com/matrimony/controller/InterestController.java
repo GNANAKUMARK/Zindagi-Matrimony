0b8242b9e568ccd5aad6970ff9bf4b6b1b872a3b
Malakayya <nandikolm@gmail.com>
2019-06-07 12:55:41 +0530

    Controller

--- src/main/java/com/matrimony/controller/InterestController.java
+++ src/main/java/com/matrimony/controller/InterestController.java
@@ -1,10 +1,14 @@
 package com.matrimony.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matrimony.entity.ProfileInterest;
 import com.matrimony.service.InterestService;
 
 
@@ -16,6 +20,12 @@ public class InterestController {
 	
 	@Autowired
 	InterestService service;
+	
+	public ResponseEntity<List<ProfileInterest>> getPendingRequests(){
+		return null;
+		
+		
+	}
 	
 
 }
